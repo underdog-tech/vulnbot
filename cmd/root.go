ca7acce420ae0cb425f8bd440684975020640a74
krishnaduttPanchagnula <krishnadutt123@gmail.com>
2023-05-29 16:19:31 +0530

    added quiet flag

--- cmd/root.go
+++ cmd/root.go
@@ -19,11 +19,13 @@ Vulnbot empowers developers and security teams to efficiently manage and respond
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	log := logger.Get()
+	if !quietFlagValue {
+		log := logger.Get()
 
-	err := rootCmd.Execute()
-	if err != nil {
-		log.Fatal().Err(err).Msg("Failed to execute command.")
+		err := rootCmd.Execute()
+		if err != nil {
+			log.Fatal().Err(err).Msg("Failed to execute command.")
+		}
 	}
 }
 
