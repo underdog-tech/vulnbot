8225876059e17b50035f746cba7e6c427c38eb53
Joey Wilhelm <wilhelm@pinwheelapi.com>
2023-07-21 09:45:33 -0600

    feat: Add tests for findings

--- querying/project_test.go
+++ querying/project_test.go
@@ -28,3 +28,43 @@ func TestProjectsAreNotDuplicated(t *testing.T) {
 	assert.Len(t, projects.Projects, 1)
 	assert.Equal(t, proj1, proj2)
 }
+
+func TestGetFindingAddsToProject(t *testing.T) {
+	project := querying.NewProject("Improbability Drive")
+	assert.Len(t, project.Findings, 0)
+	finding := project.GetFinding(
+		querying.FindingIdentifierMap{
+			querying.FindingIdentifierCVE: "CVE-42",
+		},
+	)
+	assert.Len(t, project.Findings, 1)
+	assert.Equal(t, finding, project.Findings[0])
+}
+
+func TestFindingsAreNotDuplicated(t *testing.T) {
+	project := querying.NewProject("Improbability Drive")
+	identifiers := querying.FindingIdentifierMap{
+		querying.FindingIdentifierCVE: "CVE-42",
+	}
+	finding1 := project.GetFinding(identifiers)
+	finding2 := project.GetFinding(identifiers)
+	assert.Len(t, project.Findings, 1)
+	assert.Equal(t, finding1, finding2)
+}
+
+func TestFindingIdentifiersAreMerged(t *testing.T) {
+	project := querying.NewProject("Improbability Drive")
+	id_single := querying.FindingIdentifierMap{
+		querying.FindingIdentifierCVE: "CVE-42",
+	}
+	id_multi := querying.FindingIdentifierMap{
+		querying.FindingIdentifierCVE:  "CVE-42",
+		querying.FindingIdentifierGHSA: "GHSA-4242",
+	}
+	finding1 := project.GetFinding(id_single)
+	finding2 := project.GetFinding(id_multi)
+	assert.Len(t, project.Findings, 1)
+	assert.Equal(t, finding1, finding2)
+	assert.Equal(t, finding1.Identifiers, id_multi)
+	assert.Equal(t, project.Findings[0].Identifiers, id_multi)
+}
