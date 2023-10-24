c98d4d785c7265187a5718b520284708f85d1288
Joey Wilhelm <wilhelm@pinwheelapi.com>
2023-10-24 10:31:00 -0600

    fix: Fix tests and make the new config work properly
    * Using the `reflect` package to set automatic defaults for all config
      values
    * Because of the above, we should be able to load all config values from
      the environment
    * The `ConfigFileNotFoundError` does not work as expected when
      `SetConfigFile` is used, so I created a utility function instead. ref: https://github.com/spf13/viper/issues/1491

--- config/config.go
+++ config/config.go
@@ -2,6 +2,8 @@ package config
 
 import (
 	"fmt"
+	"os"
+	"reflect"
 	"strings"
 
 	"github.com/underdog-tech/vulnbot/logger"
@@ -28,10 +30,47 @@ type Config struct {
 	Team                  []TeamConfig
 }
 
+func fileExists(fname string) bool {
+	if _, err := os.Stat(fname); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
+
 func GetUserConfig(configFile string) (Config, error) {
 	log := logger.Get()
 
 	userCfg := Config{}
+
+	// Use reflection to register all config fields in Viper to set up defaults
+	cfgFields := reflect.ValueOf(userCfg)
+	cfgType := cfgFields.Type()
+
+	for i := 0; i < cfgFields.NumField(); i++ {
+		viper.SetDefault(cfgType.Field(i).Name, cfgFields.Field(i).Interface())
+	}
+
+	// Load the main config file
+	if !fileExists(configFile) {
+		log.Fatal().Str("config", configFile).Msg("Config file not found.")
+	}
+	viper.SetConfigFile(configFile)
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatal().Err(err).Msg("Error reading config file.")
+	}
+
+	// (Optionally) Load a .env file
+	if fileExists("./.env") {
+		viper.SetConfigFile("./.env")
+		viper.SetConfigType("env")
+		if err := viper.ReadInConfig(); err != nil {
+			log.Error().Err(err).Msg("Error loading .env file.")
+		}
+	} else {
+		log.Warn().Msg("No .env file found; not loaded.")
+	}
 
 	// Set up env var overrides
 	replacer := strings.NewReplacer("-", "_")
@@ -39,27 +78,7 @@ func GetUserConfig(configFile string) (Config, error) {
 	viper.SetEnvPrefix("vulnbot")
 	viper.AutomaticEnv()
 
-	// Load the main config file
-	viper.SetConfigFile(configFile)
-	if err := viper.ReadInConfig(); err != nil {
-		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			log.Fatal().Str("config", configFile).Err(err).Msg("Config file not found.")
-		} else {
-			log.Fatal().Err(err).Msg("Error reading config file.")
-		}
-	}
-	viper.Unmarshal(&userCfg)
-
-	// (Optionally) Load a .env file
-	viper.SetConfigFile("./.env")
-	viper.SetConfigType("env")
-	if err := viper.ReadInConfig(); err != nil {
-		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			log.Warn().Msg("No .env file found; not loaded.")
-		} else {
-			log.Error().Err(err).Msg("Error loading .env file.")
-		}
-	}
+	// Finally, copy all loaded values into the config object
 	viper.Unmarshal(&userCfg)
 
 	return userCfg, nil
